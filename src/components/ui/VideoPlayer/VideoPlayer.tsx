fb86c7d1a0f5ed533ebb2787ef572fcdf6e8f2b8
Stas Khmylko <metallstas@mail.ru>
2025-03-17 15:38:34 +0300

    fix

--- src/components/ui/VideoPlayer/VideoPlayer.tsx
+++ src/components/ui/VideoPlayer/VideoPlayer.tsx
@@ -7,6 +7,7 @@ const VideoPlayer = () => {
   
     useEffect(() => {
       const dataUrl = window.location.href;
+      console.log(dataUrl)
       fetch(
         '//pleer.videoplayers.club/get_player?w=610&h=370&type=widget&kp_id=&players=videocdn,hdvb,bazon,alloha,ustore,kodik,trailer,torrent&r_id=videoplayers&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru=' +
           dataUrl,
